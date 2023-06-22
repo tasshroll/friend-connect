9603d18cd2ebda82c34cf035b553828d55d3a8bd
Tifni Shroll <tasshroll@gmail.com>
2023-06-22 15:19:02 -0600

    create models and create/delete user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,25 +6,25 @@ const userController = {
         try {
             const user = await User.create(req.body);
             res.json(user);
-          } catch (err) {
+        } catch (err) {
             res.status(500).json(err);
-          }
+        }
     },
     // Get All Users
-    async getUsers(req,res){
+    async getUsers(req, res) {
         try {
-            const users = await User.find();     
-      
+            const users = await User.find();
+
             res.json(users);
-          } catch (err) {
+        } catch (err) {
             console.log(err);
             return res.status(500).json(err);
-          }
+        }
     },
     // Get single User by ID
     async getSingleUser(req, res) {
         try {
-            const user = await User.findOne({ _id : req.params.userId});
+            const user = await User.findOne({ _id: req.params.userId });
             res.json(user);
         } catch (err) {
             console.log(err);
@@ -32,10 +32,15 @@ const userController = {
         }
     },
     // Delete a User
-    async deleteUser (req, res) {
-        const user = 0;
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({ _id: req.params.userId });
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
     }
-
 }
 
 
